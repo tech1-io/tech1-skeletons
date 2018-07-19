548acf7f93150db782ab3eb17a9dda96379ffb4f
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2018-07-19 23:21:07 +0300

    add support, Java8, folders for spring boot devops skeleton

--- devops-skeletons/springBootOnUbuntu.sh
+++ devops-skeletons/springBootOnUbuntu.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-# to be continued...
+# Remote: support components
+sudo apt-get update && \
+sudo apt-get install -y software-properties-common && \
+sudo apt-get clean && \
+sudo rm -rf /var/lib/apt/lists
+
+# Remote: Java8
+sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && \
+sudo add-apt-repository -y ppa:webupd8team/java && \
+sudo apt-get update && \
+sudo apt-get install -y oracle-java8-installer && \
+sudo rm -rf /var/lib/apt/lists/* && \
+sudo rm -rf /var/cache/oracle-jdk8-installer
+
+# Remote: Folders
+mkdir /root/preparation
+mkdir /root/app
+
