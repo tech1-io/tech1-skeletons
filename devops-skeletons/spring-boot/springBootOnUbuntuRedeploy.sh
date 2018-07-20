982d8966e21a96e8d87cef6eebf4354141d37c5f
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2018-07-20 14:39:50 +0300

    -

---
+++ devops-skeletons/spring-boot/springBootOnUbuntuRedeploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Remote: stop service
+systemctl stop app.service
+
+# Remote: prepare JAR
+cp /root/preparation/app.jar /root/app/app.jar
+chmod +x /root/app/app.jar
+
+# Remote: start service
+systemctl start app.service
