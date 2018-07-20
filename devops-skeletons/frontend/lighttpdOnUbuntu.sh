07e42d17841465096beca2779f445c89eb9b1187
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2018-07-20 21:53:17 +0300

    add server configuration

--- devops-skeletons/frontend/lighttpdOnUbuntu.sh
+++ devops-skeletons/frontend/lighttpdOnUbuntu.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # Remote: lighttpd
-apt-get update
-apt-get upgrade 
-apt-get install -y lighttpd
+apt-get update && \
+apt-get install -y lighttpd && \
+apt-get clean 
+
+# Remote: Override server configuration
+cp lighttpd.conf /etc/lighttpd/lighttpd.conf
 
 # Remote: Cleanup
 cd /root
