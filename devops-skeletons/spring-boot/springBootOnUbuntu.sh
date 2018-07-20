0ae9056503d6f7c672afc8214db645157779acf2
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2018-07-20 09:22:02 +0300

    check default permission thought Jenkins

--- devops-skeletons/spring-boot/springBootOnUbuntu.sh
+++ devops-skeletons/spring-boot/springBootOnUbuntu.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # Remote: support components
-sudo apt-get update && \
-sudo apt-get install -y software-properties-common && \
-sudo apt-get clean && \
-sudo rm -rf /var/lib/apt/lists
+apt-get update && \
+apt-get install -y software-properties-common && \
+apt-get clean && \
+rm -rf /var/lib/apt/lists
 
 # Remote: Java8
-sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && \
-sudo add-apt-repository -y ppa:webupd8team/java && \
-sudo apt-get update && \
-sudo apt-get install -y oracle-java8-installer && \
-sudo rm -rf /var/lib/apt/lists/* && \
-sudo rm -rf /var/cache/oracle-jdk8-installer
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \
+add-apt-repository -y ppa:webupd8team/java && \
+apt-get update && \
+apt-get install -y oracle-java8-installer && \
+rm -rf /var/lib/apt/lists/* && \
+rm -rf /var/cache/oracle-jdk8-installer
 
 # Remote: Folders
 mkdir /root/preparation
