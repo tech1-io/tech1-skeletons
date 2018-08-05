70c8eb57c080837f679da764664487359100c494
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2018-08-05 19:24:01 +0300

    complete profiles as attributes (vuejs-***,spring-boot-***)

--- devops-skeletons/docker/spring-boot/dockerRun.sh
+++ devops-skeletons/docker/spring-boot/dockerRun.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker run -it -p 8484:8484 \
--e SPRING_BOOT_PROFILE='dev' \
---rm --name app-server forcelate/spring-boot
+  -e SPRING_BOOT_PROFILE='dev' \
+  --rm --name app-server forcelate/spring-boot
--- devops-skeletons/docker/vuejs-http-server/dockerRun.sh
+++ devops-skeletons/docker/vuejs-http-server/dockerRun.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker run -it -p 8787:8080 --rm --name app-client forcelate/vuejs-http-server
+docker run -it -p 8787:8080 \
+  -e VUEJS_PROFILE='dev' \
+  --rm --name app-client forcelate/vuejs-http-server
