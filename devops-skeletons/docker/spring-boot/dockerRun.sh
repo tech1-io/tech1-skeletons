841e3862afebad8bb07a6acea512d63bc87b9a5e
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2018-08-05 18:44:29 +0300

    add docker: spring-boot based (add profile)

--- devops-skeletons/docker/spring-boot/dockerRun.sh
+++ devops-skeletons/docker/spring-boot/dockerRun.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker run -it -p 8484:8484 --rm --name app-server forcelate/spring-boot
+docker run -it -p 8484:8484 \
+-e SPRING_BOOT_PROFILE='dev' \
+--rm --name app-server forcelate/spring-boot
