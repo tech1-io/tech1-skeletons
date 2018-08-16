0c682f4961c333c94dab6c5964baae9de9521cfa
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2018-08-16 10:02:53 +0300

    add jenkins-based skeletons

---
+++ devops-skeletons/docker/spring-boot/jenkins-redeploy.sh
@@ -0,0 +1,11 @@
+COMPANY=forcelate
+APP=vuejs-nginx
+TAG=latest
+
+docker stop ${APP}
+
+docker pull ${COMPANY}/${APP}:${TAG}
+
+docker run -d -it -p 8484:8484 \
+  -e SPRING_BOOT_PROFILE=${SPRING_BOOT_PROFILE} \
+  --rm --name ${APP} ${COMPANY}/${APP}:${TAG}
