20b369b16466a3ad824617a3c5a70b2ee93d0164
Pavlo Pastushok <pavlo.pastushok@gmail.com>
2018-09-06 11:33:38 +0300

    Update jenkins-redeploy.sh
    added logs volume

--- devops-skeletons/docker/spring-boot/jenkins-redeploy.sh
+++ devops-skeletons/docker/spring-boot/jenkins-redeploy.sh
@@ -10,5 +10,6 @@ docker stop ${APP}
 docker pull ${COMPANY}/${APP}:${TAG}
 
 docker run -d -it -p 8484:8484 \
+  -v /root/app/logs:/root/app/logs \
   -e SPRING_BOOT_PROFILE=${SPRING_BOOT_PROFILE} \
   --rm --name ${APP} ${COMPANY}/${APP}:${TAG}
