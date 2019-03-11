be2f0fc037d777ae026e1e1f63fe4c1628aeb772
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2019-03-11 09:19:09 +0200

    add utils as interface/impl + tests

--- spring-boot-default-skeleton/src/main/java/com/forcelate/utils/EnvironmentUtils.java
+++ spring-boot-default-skeleton/src/main/java/com/forcelate/utils/EnvironmentUtils.java
@@ -1,23 +1,9 @@
 package com.forcelate.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 import java.util.List;
 
-@Component
-public class EnvironmentUtils {
-
-    private final Environment environment;
-
-    @Autowired
-    public EnvironmentUtils(Environment environment) {
-        this.environment = environment;
-    }
-
-    public List<String> getActiveProfiles() {
-        return Arrays.asList(this.environment.getActiveProfiles());
-    }
+public interface EnvironmentUtils {
+    List<String> getActiveProfiles();
+    boolean isDevelopmentMode();
+    boolean isProductionMode();
 }
