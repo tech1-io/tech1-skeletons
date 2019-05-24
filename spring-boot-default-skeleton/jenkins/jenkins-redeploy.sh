8f057fad3f43fb57ec6597f056579ec6b6efca96
Pavlo Pastushok <pavlo.pastushok@gmail.com>
2019-05-24 10:03:16 +0300

    updated jenkins scripts with restart function

--- spring-boot-default-skeleton/jenkins/jenkins-redeploy.sh
+++ spring-boot-default-skeleton/jenkins/jenkins-redeploy.sh
@@ -6,10 +6,12 @@ TAG=latest
 SPRING_BOOT_PROFILE=prod
 
 if [ "$(docker ps -q -f name=${APP})" ]; then docker kill ${APP}; else echo "Container ${APP} is missing"; fi
+docker rm ${APP}
 
 docker pull ${COMPANY}/${APP}:${TAG}
 
 docker run -d -it -p 8484:8484 \
 	-v /root/app/logs:/root/app/logs \
  	-e SPRING_BOOT_PROFILE=${SPRING_BOOT_PROFILE} \
-  	--rm --name ${APP} ${COMPANY}/${APP}:${TAG}
+ 	--restart unless-stopped \
+  	--name ${APP} ${COMPANY}/${APP}:${TAG}
--- spring-boot-default-skeleton/jenkins/jenkins-stop.sh
+++ spring-boot-default-skeleton/jenkins/jenkins-stop.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-COMPANY=forcelate
 APP=spring-boot-server
-TAG=latest
 
 if [ "$(docker ps -q -f name=${APP})" ]; then docker kill ${APP}; else echo "Container ${APP} is missing"; fi
+docker rm ${APP}
