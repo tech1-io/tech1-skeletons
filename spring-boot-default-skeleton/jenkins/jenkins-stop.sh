900f4cf7e6a18ae8ddf563f21a915e0328a3f6cb
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2019-09-10 17:33:35 +0300

    migrate script to devtools

--- spring-boot-default-skeleton/jenkins/jenkins-stop.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-APP=spring-boot-server
-
-if [ "$(docker ps -q -f name=${APP})" ]; then docker kill ${APP}; else echo "Container ${APP} is missing"; fi
-if [ "$(docker ps -q -a -f name=${APP})" ]; then docker rm ${APP}; else echo "Container ${APP} is missed."; fi
